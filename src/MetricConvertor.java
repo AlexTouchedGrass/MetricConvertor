29997c223a3995a86e020d8f9034e0f904f8e64f
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-10-12 09:46:17 -0400

    WOW

--- src/MetricConvertor.java
+++ src/MetricConvertor.java
@@ -1,5 +1,47 @@
-public class Main {
+import java.util.Scanner;
+import java.lang.Math;
+
+public class MetricConvertor {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        double measureMile = 0;
+        double measureFeet = 0;
+        double measureInch = 0;
+        double measureMeter = 0;
+        final double mathMile = 0.00062137;
+        final double mathFeet = 3.28084;
+        final double mathInch = 39.37008;
+
+
+        boolean valid = true;
+
+        //DO WHILE FOR CHECKING METERS
+
+        do {
+            System.out.println("Please enter a value for meters below. \n(Cannot be negative or a letter)");
+
+            if (scan.hasNextDouble()){
+                measureMeter = scan.nextDouble();
+
+                //Buffer
+                scan.nextLine();
+
+                if (measureMeter > 0){
+                    valid = false;
+                } else {
+                    System.out.println("Your math ain't mathin'.\nThat is a zero or a negative.");
+                }
+
+            } else {
+                System.out.println("Jesus, come on man that is a letter.");
+            }
+        } while (valid);
+
+        measureMile = measureMeter * mathMile;
+        measureFeet = measureMeter * mathFeet;
+        measureInch = measureInch * mathInch;
+
+
     }
 }
